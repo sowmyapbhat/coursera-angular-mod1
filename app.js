c75421deb63d90c16b760996d724065b2d94df0d
Sowmya <Panjaje@Panjajes-MacBook-Air.local>
2017-01-14 08:31:47 +0530

    Comitting First Assignment

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 (function(){
+  'use strict';
+angular.module("LunchCheck",[])
+.controller("LunchCheckController", LunchCheckController);
 
+LunchCheckController.$inject=['$scope', '$filter'];
+function LunchCheckController($scope, $filter){
+  $scope.dishes = "";
+  $scope.message = "";
+  $scope.msgColor = "";
+  $scope.inpColor = "";
+
+  $scope.check = function() {
+    var len = $scope.dishes.split(",");
+    len = len.filter(function(elem){
+      return elem.trim() !="";
+    }).length;
+
+    $scope.msgColor = 'Green';
+    $scope.inpColor = 'Green';
+
+    if (len > 3)
+      $scope.message = "Too much!";
+    else if (len > 0)
+      $scope.message = "Enjoy!";
+    else {
+      $scope.message = "Please enter data first";
+      $scope.msgColor = 'Red';
+      $scope.inpColor = 'Red';
+    }
+  };
+}
 })();
